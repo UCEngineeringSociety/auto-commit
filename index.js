9e5c3d792c27cec2af9b9a7571815f44bb814987
Alex <chess2ryme@gmail.com>
2017-09-05 15:53:14 +1000

    Commit Number: #0

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-var sys = require('util')
-var exec = require('child_process').exec;
-var child;
+var execSync = require('child_process').execSync
+var fs = require('fs')
+var index = 0
 
-child = exec("pwd", function (error, stdout, stderr) {
-  console.log(stdout);
-});
+function commit (message) {
+  var child = execSync('git add . && git commit -m "' + message + '" && git push -u origin master', function (error, stdout, stderr) {
+    console.log(stdout)
+  })
+}
+
+function changeFile () {
+  var newValue = 'Commit Number ' + index
+
+  fs.writeFileSync('commit.txt', newValue, 'utf-8')
+
+  console.log('Commit ' + index + ' inserted.')
+}
+
+function startAutoCommit () {
+  var seconds = Math.floor(Math.random() * 20) + 10
+  commit('Commit Number: #' + index)
+  changeFile()
+  index++
+
+  setTimeout(function () {
+    startAutoCommit()
+  }, seconds * 1000)
+}
+
+startAutoCommit()
